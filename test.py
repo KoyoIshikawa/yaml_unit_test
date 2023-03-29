42b942cd0ade50d85c44bcf184624c3082d8b88d
KoyoIshikawa <koyoi561@gmail.com>
2023-03-29 11:13:51 +0900

    値をわかりやすい名称に変更。

--- test.py
+++ test.py
@@ -10,23 +10,26 @@ path = 'output.txt'
 with open(path) as f:
     s = f.read()
 
+# output.txtを一行ずつ分解し,list化
 lines = s.splitlines()
 
-print(lines)
-print("========")
+# output.txtの値がなくなるまで実行
 for line in lines:
-    left = line.split('=')[0]
-    right = line.split('=')[1]
-    aaa = ast.literal_eval(left)
-    print(aaa)
-    print("==")
+    # output.txtから値を取得
+    key_chain = line.split('=')[0]
+    expected_value = line.split('=')[1]
+    # key_cahinをlist型に変換
+    listing_key_chain = ast.literal_eval(key_chain)
+    # valueの初期化
     value = file.copy()
-    for element in aaa:
+
+    # listing_key_chainがなくなるまでvalueに内容をvalueを上書き
+    for element in listing_key_chain:
         value = value[element]
 
     print("yamlの値:{}".format(value))
-    print("期待値:{}".format(right))
-    if value == right:
+    print("期待値:{}".format(expected_value))
+    if value == expected_value:
         print("OK")
     else:
         print("NG")
